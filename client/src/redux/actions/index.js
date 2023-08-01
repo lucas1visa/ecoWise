246e49411cd52617b08e29447c136c7c27df78f6
lucas1visa <visalucas557@gmail.com>
2023-08-01 15:38:45 -0300

    react-router-dom

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -8,9 +8,10 @@ import {GETUSERS} from "./Types"
 
 export const getUsers = ()=>{
     return async (dispatch)=>{
-        const {data} = axios.get("/users")
+        const {data} = await axios.get("/users")
         console.log(data)
         dispatch({ type:GETUSERS ,payload: data})
     }
 }
-getUsers()
+
+console.log(getUsers())
