e2f24c5e0f3dabb2880b947533908d06cd79b81a
lucas1visa <visalucas557@gmail.com>
2023-07-31 23:42:03 -0300

    Ruta /User

---
+++ server/src/handlers/handlersUsers.js
@@ -0,0 +1,32 @@
+const {users,crearUsers,update}=require("../controllers/controllerUsers")
+const getUsers = async(req,res)=>{
+    try {
+        todosLosUsuarios = await users()
+        res.status(200).send(todosLosUsuarios)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+}
+
+const postUsers = async(req,res)=>{
+    const {name,email,password} = req.body
+    try {
+        const crearUsuario = crearUsers(name,email,password)
+        res.status(200).send("Se Registro Correctamente")
+    } catch (error) {
+        
+    }
+}
+
+
+const putUsers = async (req, res) => {
+    const { password,id} = req.body;
+    const updateUser =await update(id,password)
+    try {
+        res.status(200).send("Usuario actualizado correctamente");
+    } catch (error) {
+        res.status(500).send("Hubo un error al actualizar el usuario");
+    }
+};
+
+module.exports= {getUsers,postUsers,putUsers}
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,5 +1,7 @@
 const { Router } = require("express");
+const routesUsers = require("./routesUsers")
 //comentario
 const router = Router();
+router.use("/users", routesUsers)
 
 module.exports = router;
