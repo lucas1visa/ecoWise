d103866d9be8f235fc48ff08de3d52a65ae76414
lucas1visa <visalucas557@gmail.com>
2023-08-01 23:48:41 -0300

    actualizacion

--- client/src/component/Home/Home.jsx
+++ client/src/component/Home/Home.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
 const Home = () => {
-    return (
+  const products = useSelector((state) => state.products); 
+
+  return (
     <div>
-    <Navbar/>
-    {/* <Footer/> */}
-    </div>  );
-}
- 
+      <div >
+        {products.map((product) => (
+          <div key={product.id}>
+            <Link to={`/products/${product.id}`}>
+              <img src={product.image} alt={product.name}  />
+            </Link>
+            <h3>{product.name}</h3>
+            <p>{product.description}</p>
+            <p>Precio: ${product.price}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
 export default Home;
