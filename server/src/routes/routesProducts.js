984d8294086a3f1190db1e788fa2e999986d7fb5
lucas1visa <visalucas557@gmail.com>
2023-08-01 23:34:15 -0300

    bug de rutas okey

--- server/src/routes/routesProducts.js
+++ server/src/routes/routesProducts.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const {getProducts, postProducts, putProducts, deleteProduct,getProductByName,getProductById} = require("../handlers/handlersProducts")
 const  routesProducts = Router();
 routesProducts.get("/" ,getProducts)
-routesProducts.get("/:id" ,getProductById)
+routesProducts.get("/search/:id" ,getProductById)
 routesProducts.get("/search", getProductByName);
 routesProducts.post("/", postProducts)
 routesProducts.put("/update", putProducts)
