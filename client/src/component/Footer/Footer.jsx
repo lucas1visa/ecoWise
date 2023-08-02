df7ea14d5a7d525d5ded2aa45df392dabd9277f5
lucas1visa <visalucas557@gmail.com>
2023-08-02 15:29:43 -0300

    footer

--- client/src/component/Footer/Footer.jsx
+++ client/src/component/Footer/Footer.jsx
@@ -5,7 +5,9 @@ const Footer = () => {
     <footer className="bg-dark text-white text-center py-3">
       <div className="container">
         <p>&copy; {new Date().getFullYear()} EcoWise. Todos los derechos reservados.</p>
+        <h1>s</h1>
       </div>
+      
     </footer>
   );
 };
