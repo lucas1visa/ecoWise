e218fb2e7d692114003324883c76f97b7997bdfe
lucas1visa <visalucas557@gmail.com>
2023-08-02 16:48:09 -0300

    lucas1visa
    mercado pago

---
+++ server/src/utils/mercadopago.js
@@ -0,0 +1,14 @@
+
+const mercadopago = require("mercadopago");
+require("dotenv").config
+mercadopago.configure({
+  access_token: "PROD_ACCESS_TOKEN",
+});
+
+
+
+module.exports= {
+    mercadopago
+}
+
+
