590cb5f9a6a928e37b63198f88673cf8edaabe9a
Carla Yamila Colom <121466242+CarColom@users.noreply.github.com>
2023-08-02 20:09:37 -0300

    Redux - detail
    actions favorites, carrito, id  - detail

--- client/src/component/ProductDetail/ProductDetail.jsx
+++ client/src/component/ProductDetail/ProductDetail.jsx
@@ -1,18 +1,21 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { addToFavorites, addToCart } from '../actions'; 
-import Navbar from "../Navbar"
-import Footer from "../Footer"
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addFav, addToCart, getId } from "../../redux/actions/index";
+import { useParams } from 'react-router-dom';
 
+const ProductDetail = () => {
+  const { id } = useParams();
 
-const ProductDetail = ({ props }) => { 
-  const { productId } = props.params; //// Extrae el valor del parámetro "productId" de las props del componente
-  const product = useSelector(state => state.products.find(product => product.id === productId));// Obtiene el producto correspondiente al "productId" 
+  const product = useSelector((state) => state.products);
   const dispatch = useDispatch();
   const [quantity, setQuantity] = useState(1);
 
+  useEffect(() => {
+    dispatch(getId(id));
+  }, [dispatch, id]);
+
   const handleAddToFavorites = () => {
-    dispatch(addToFavorites(product.id));
+    dispatch(addFav(product.id));
   };
 
   const handleAddToCart = () => {
@@ -20,37 +23,47 @@ const ProductDetail = ({ props }) => {
   };
 
   return (
-     <div >
-<Navbar/>
-   <main>
-      <img src={product.image} alt={product.name} />
-      <h2>{product.name}</h2>
-      <p>{product.description}</p>
-      <p>Price: ${product.price}</p>
-      <p>Quantity available: {product.quantityAvailable}</p>
-      <p>Category: {product.category}</p>
-      <button onClick={handleAddToFavorites}>
-      ❤️ 
-      </button>
-      <div>
-      <label htmlFor="quantity-select">Quantity:</label>
-  <select
-    id="quantity-select"
-    value={quantity}
-    onChange={(e) => setQuantity(Number(e.target.value))}
-  >
-    {Array.from({ length: product.quantityAvailable }, (_, index) => index + 1).map((count) => ( // array con la cantidad de elementos igual a product.quantityAvailable. map  genera las opciones dentro del selector.
-      <option key={count} value={count} disabled={count > product.quantityAvailable}>{count}</option>//value= valor de la opción, disabled=deshabilita las opciones donde count (la cantidad seleccionada) sea mayor que product.quantityAvailable
-    ))}
-  </select>
-        <button onClick={handleAddToCart}>
-          Add to Cart
-        </button>
-      </div>
+    <div>
+      <main>
+        <img src={product.image} alt={product.name} />
+        <h2>{product.name}</h2>
+        <p>{product.description}</p>
+        <p>Price: ${product.price}</p>
+        <p>Quantity available: {product.quantityAvailable}</p>
+        <p>Category: {product.category}</p>
+        <button onClick={handleAddToFavorites}>❤️</button>
+        <div>
+          <label htmlFor="quantity-select">Quantity:</label>
+          <select
+            id="quantity-select"
+            value={quantity}
+            onChange={(e) => setQuantity(Number(e.target.value))}
+          >
+            {Array.from(
+              { length: product.quantityAvailable },
+              (_, index) => index + 1
+            ).map(
+              (
+                count // array con la cantidad de elementos igual a product.quantityAvailable. map  genera las opciones dentro del selector.
+              ) => (
+                <option
+                  key={count}
+                  value={count}
+                  disabled={count > product.quantityAvailable}
+                >
+                  {count}
+                </option> //value= valor de la opción, disabled=deshabilita las opciones donde count (la cantidad seleccionada) sea mayor que product.quantityAvailable
+              )
+            )}
+          </select>
+          <button onClick={handleAddToCart}>Add to Cart</button>
+        </div>
       </main>
-      <Footer />
+
     </div>
   );
 };
+
+
 
 export default ProductDetail;
