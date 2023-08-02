dd82fea7b829bf6dce381a2761c41eb5c79bd2df
lucas1visa <visalucas557@gmail.com>
2023-08-01 22:47:35 -0300

    Funcionando Todo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,15 @@ import { useState } from 'react'
 import './App.css'
 import axios from "axios";
 axios.defaults.baseURL = "http://localhost:3001";
+import Home from './component/Home/Home';
 
 function App() {
 
   return (
     <>
+      <Routes>
+      <Route path="/home" element={<Home />} />
+      </Routes>
     </>
   )
 }
