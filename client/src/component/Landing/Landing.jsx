789166356ac6a6be9844bc717d064633d064f5d0
sebasimon9 <sebasimon9@gmail.com>
2023-08-03 10:22:15 -0300

    Landing, falta agregar un color de fondo

--- client/src/component/Landing/Landing.jsx
+++ client/src/component/Landing/Landing.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 
 const Landing = () => {
     return (
-      <main className="container">
-        <section className="row my-5">
+      <main className="container-fluid">
+        <section className="row my-6">
           <div className="col-md-6">
             <div className="mb-4">
               <h1 className="display-4">EcoWise</h1>
@@ -15,10 +15,10 @@ const Landing = () => {
               <p>
                 Esta app nace con el propósito de fomentar el cuidado del medio ambiente. Nuestro objetivo es lograr reemplazar la utilización del plástico de un solo uso comercializando productos con materiales alternativos sustentables. Desde EcoWise consideramos que cambiar hábitos cotidianos, es generar cambios para un futuro sostenible, logrando “contagiar” a otras personas para impulsar un movimiento mayor.
               </p>
-              <Link to="/home" className="btn btn-primary mr-3">Log in</Link>
-              <Link to="/account/register/" className="btn btn-secondary">Sign in</Link>
+              <Link to="/home" class="btn btn-outline-success">Log in</Link>
+              <Link to="/home" class="btn btn-outline-success">Invitado</Link>
               <div>
-              <Link to="/home" className='"btn btn-primary mr-3'>Invitado</Link>
+              <Link to="/account/register/" class="btn btn-success">Sing in</Link>
               </div>
             </div>
           </div>
