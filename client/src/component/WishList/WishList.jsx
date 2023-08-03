abfe7e49adcdfa6b055fce870c69423a7618f5fe
Carla Yamila Colom <121466242+CarColom@users.noreply.github.com>
2023-08-03 13:00:09 -0300

    favorites

--- client/src/component/WishList/WishList.jsx
+++ client/src/component/WishList/WishList.jsx
@@ -1,17 +1,12 @@
 
-import { useSelector } from 'react-redux';
+
 
 const WishList = () => {
-  const favoriteProducts = useSelector((state) => state.favorites);
+  
 
   return (
     <div>
-      <h2>WishList</h2>
-      <ul>
-        {favoriteProducts.map((product) => (
-          <li key={product.id}>{product.name}</li>
-        ))}
-      </ul>
+    
     </div>
   );
 };
