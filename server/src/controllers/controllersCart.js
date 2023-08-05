8c6251a8697c15cd8b4c7a226921d47c9871cdf7
Jonathan Jaramillo <125410451+JonathanJara01@users.noreply.github.com>
2023-08-05 00:09:56 -0500

    Controllers Carrito
    se crearon los controllers del carrito de compras

--- server/src/controllers/controllersCart.js
+++ server/src/controllers/controllersCart.js
@@ -1,12 +1,54 @@
-const getcarrito = ()=>{
+const Cart = require("../models/Cart");
 
+const getcarrito = async ()=>{
+    try {
+        const productosEnCarrito = await Cart.findAll();
+        return productosEnCarrito;
+      } catch (error) {
+        console.error("Error al obtener los Productos del carrito de compras:", error);
+        return null;
+      }
 }
 
-const crearCart = ()=>{
-
+const crearCart = async (name, price, quantityAvailable, image)=>{
+    try {
+    const nuevoProductoCarrito = await Cart.create({
+        name,
+        price,
+        quantityAvailable,
+        image,
+      });
+  
+      return nuevoProductoCarrito;
+    } catch (error) {
+      console.error("Error el producto al carrito de compras:", error);
+      return null;
+    }
 }
 
-
-const updateCarrito = ()=>{
-
+// no incluyo el nombre porque siempre va a ser el mismo nombre
+const updateCarrito = async (  id, price, quantityAvailable, image)=>{
+    try {
+        const cargaDeDatosNuevos = await Cart.update(
+          {
+            price: price,
+            quantityAvailable: quantityAvailable,
+            image: image,
+          },
+          {
+            where: {
+              id: id,
+            },
+          }
+        );
+        return cargaDeDatosNuevos
+      } catch (error) {
+        throw error;
+      }
 }
+
+module.exports = {
+getcarrito,
+crearCart,
+updateCarrito
+  };
