032986ddac1a66987cdb06719445760b050fa45a
lucas1visa <visalucas557@gmail.com>
2023-08-05 00:03:41 -0300

    agregado

--- server/src/routes/routesCart.js
+++ server/src/routes/routesCart.js
@@ -1,11 +1,11 @@
 const { Router } = require("express");
-
+const { putCart,postCart,getCart} = require("../handlers/handlersCart")
 const routesCart = Router();
 
 
-routesCart.get("/",handlersGetCart)
-routesCart.post("/",handlersGetCart)
-routesCart.put("/",handlersGetCart)
+routesCart.get("/",getCart)
+routesCart.post("/",postCart)
+routesCart.put("/",putCart)
 
 
 module.exports = routesCart
